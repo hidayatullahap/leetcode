9da5d1474226cf98cd7d886c89c044964f7802a6
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2025-12-30 08:56:40 +0700

    fix  solution

--- src/twosum.ts
+++ src/twosum.ts
@@ -1,10 +1,21 @@
-export function twoSum(nums: number[], target: number): number[] {
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+export function twoSum1(nums: number[], target: number): number[] {
     for (let i = 0; i < nums.length; i++) {
-        const sum = nums[i] + nums[i + 1];
-        if (sum === target) {
+        if (nums[i] + nums[i + 1] === target) {
             return [i, i + 1]
         }
 
+        for (let j = 0; j < nums.length; j++) {
+            if (j === i) {
+                continue
+            }
+
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
     }
 
     return []
--- src/twosum_test.ts
+++ src/twosum_test.ts
@@ -1,8 +1,9 @@
 import { assertEquals } from "@std/assert";
-import { twoSum } from "./twosum.ts";
+import { twoSum1 } from "./twosum.ts";
 
 Deno.test(function addTest() {
-  assertEquals(twoSum([2, 7, 11, 15], 9), [0, 1]);
-  assertEquals(twoSum([3, 2, 4], 6), [1, 2]);
-  assertEquals(twoSum([3, 3], 6), [0, 1]);
+  assertEquals(twoSum1([2, 7, 11, 15], 9), [0, 1]);
+  assertEquals(twoSum1([3, 2, 4], 6), [1, 2]);
+  assertEquals(twoSum1([3, 3], 6), [0, 1]);
+  assertEquals(twoSum1([3, 2, 3], 6), [0, 2]);
 });
