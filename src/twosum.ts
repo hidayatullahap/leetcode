a425546354f8e64bf2ee6defa28b249a7882bb83
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2025-12-30 09:13:21 +0700

    add second twosum

--- src/twosum.ts
+++ src/twosum.ts
@@ -20,3 +20,19 @@ export function twoSum1(nums: number[], target: number): number[] {
 
     return []
 };
+
+export function twoSum2(nums: number[], target: number): number[] {
+    const map = new Map<number, number>()
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+
+        if (map.has(complement)) {
+            return [map.get(complement)!, i]
+        }
+
+        map.set(nums[i], i)
+    }
+
+    return [];
+};
--- src/twosum_test.ts
+++ src/twosum_test.ts
@@ -1,9 +1,16 @@
 import { assertEquals } from "@std/assert";
-import { twoSum1 } from "./twosum.ts";
+import { twoSum1, twoSum2 } from "./twosum.ts";
 
 Deno.test(function addTest() {
   assertEquals(twoSum1([2, 7, 11, 15], 9), [0, 1]);
   assertEquals(twoSum1([3, 2, 4], 6), [1, 2]);
   assertEquals(twoSum1([3, 3], 6), [0, 1]);
   assertEquals(twoSum1([3, 2, 3], 6), [0, 2]);
+});
+
+Deno.test(function addTest() {
+  assertEquals(twoSum2([2, 7, 11, 15], 9), [0, 1]);
+  assertEquals(twoSum2([3, 2, 4], 6), [1, 2]);
+  assertEquals(twoSum2([3, 3], 6), [0, 1]);
+  assertEquals(twoSum2([3, 2, 3], 6), [0, 2]);
 });
